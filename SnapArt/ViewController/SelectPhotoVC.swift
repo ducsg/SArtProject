7f9104c9ac6989c774c5b4d7ce44a968cbf65809
HD <hduc.hcm@gmail.com>
2015-11-07 13:59:15 +0700

    add select photo

--- SnapArt/ViewController/SelectPhotoVC.swift
+++ SnapArt/ViewController/SelectPhotoVC.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 import Darwin
+import Alamofire
+import SwiftyJSON
+
 class SelectPhotoVC: CustomViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIActionSheetDelegate{
     @IBOutlet weak var cropView: BaseView!
     @IBOutlet weak var sizeBtn: UIButton!
-    @IBOutlet weak var cropContainView: UIView!
+    @IBOutlet weak var containView: UIView!
     
-    private var cropV:NLImageCropperView!
     internal var imageCrop:UIImage!
     private var TITTLE = "Input Size"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = TITTLE
@@ -24,15 +26,28 @@ class SelectPhotoVC: CustomViewController ,UIImagePickerControllerDelegate, UINa
         self.sizeBtn.layer.borderWidth = 0.5
         self.sizeBtn.layer.borderColor = UIColor.grayColor().CGColor
         self.sizeBtn.backgroundColor = UIColor.whiteColor()
-        cropV = NLImageCropperView(frame: cropContainView.bounds)
-        self.cropContainView.addSubview(cropV)
-        self.cropContainView.backgroundColor = UIColor.clearColor()
-        cropV.setCropRegionRect(CGRectMake(1, 1, 600, 600))
-        imageCrop = UIImage(named: "girl_image")
-        cropV.setHiddenCropView(true)
-        cropV.setImage(imageCrop)
+        imageCrop = UIImage(named: "sample")
+       
+//        let api = Api()
+//        api.uploadCropImage(imageCrop!, resulf: {(dataResult: (success: Bool, message: String, data: JSON!)) -> Void in
+//        
+//        })
+//        api.execute(.POST, url: ApiUrl.crop_image_url, parameters: param, resulf: {(dataResult: (success: Bool, message: String, data: JSON!)) -> Void in
+//            if(dataResult.success){
+//
+//            }else{
+//                Util().showAlert(dataResult.message, parrent: self)
+//            }
+//        })
 
-        // Do any additional setup after loading the view.
+        
+        
+        //        let ratio = self.cropContainView.frame.height/self.cropContainView.frame.width
+        //        let width = self.cropContainView.frame.size.width
+        //        let height = width*ratio
+        //        let imageScale = Util().imageResize(imageCrop, sizeChange: CGSize(width: width,height: height))
+        //        let rect = CGRectMake(0, 0, width, height)
+        //        self.cropContainView.backgroundColor = UIColor.clearColor()
     }
     
     override func didReceiveMemoryWarning() {
@@ -42,26 +57,26 @@ class SelectPhotoVC: CustomViewController ,UIImagePickerControllerDelegate, UINa
     
     
     @IBAction func sizeTap(sender: AnyObject) {
-//        let cropSizes = ["3 x 2", "3 x 5", "4 x 3", "4 x 6"," 5 x 7"," 8 x 10", "16 x 9"]
-//        let sizePicker = ActionSheetStringPicker(title: "Size", rows: cropSizes, initialSelection: 0, doneBlock: {picker, value, index in
-//            let selectIndex = value as! Int
-//            self.setValueSizeBtn(cropSizes[selectIndex])
-//            self.cropVC.cropedImageSizeWithRatio(1, and: 1)
-//
-//            return }, cancelBlock: {ActionStringCancelBlock in
-//                return}, origin: sender.superview)
-//        sizePicker.showActionSheetPicker()
+        //        let cropSizes = ["3 x 2", "3 x 5", "4 x 3", "4 x 6"," 5 x 7"," 8 x 10", "16 x 9"]
+        //        let sizePicker = ActionSheetStringPicker(title: "Size", rows: cropSizes, initialSelection: 0, doneBlock: {picker, value, index in
+        //            let selectIndex = value as! Int
+        //            self.setValueSizeBtn(cropSizes[selectIndex])
+        //            self.cropVC.cropedImageSizeWithRatio(1, and: 1)
+        //
+        //            return }, cancelBlock: {ActionStringCancelBlock in
+        //                return}, origin: sender.superview)
+        //        sizePicker.showActionSheetPicker()
     }
     
     @IBAction func cropTap(sender: AnyObject) {
-
+        
     }
     
     @IBAction func continueTap(sender: AnyObject) {
         let vc = Util().getControllerForStoryBoard("CropItVC") as! CropItVC
         self.navigationController?.pushViewController(vc, animated: true)
     }
-
+    
     func setValueSizeBtn(sizeValues:String) -> Void {
         self.sizeBtn.setTitle(sizeValues, forState: .Normal)
         self.sizeBtn.setTitle(sizeValues, forState: .Highlighted)
